0cb04abcec65fa1261756d053728bae8580f2d3e
Chandan Ojha <chandanojha776@gmail.com>
2025-09-28 11:15:50 +0600

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,7 +27,7 @@
 <body>
     @inertia
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-    <script src="./assets/js/app.js"></script>
+    {{-- <script src="./assets/js/app.js"></script> --}}
     <script>
         AOS.init({
             offset: 100,
